92e1ef01d1ff8ed98b67844ff37fa6b0863fb5f2
EmbraceLife <1227561934@qq.com>
2017-05-19 16:10:15 +0800

    torch.nn.functional, torch.nn, torch.linspace, F.softmax, do basic subplot inside a figure

--- tutorial-contents/203_activation.py
+++ tutorial-contents/203_activation.py
@@ -7,28 +7,38 @@ torch: 0.1.11
 matplotlib
 """
 import torch
+# with torch.nn.functional as F, we can quickly deploy activations to use
+# to see detailed doc of activation, we need
+# import torch.nn as nn
+# doc nn.ReLU
 import torch.nn.functional as F
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
 
-# fake data
+# torch.linspace(range_start, range_end, total_num)
+# it is 1-d tensor
 x = torch.linspace(-5, 5, 200)  # x data (tensor), shape=(100, 1)
 x = Variable(x)
 x_np = x.data.numpy()   # numpy array for plotting
 
-# following are popular activation functions
+# apply activation only to a Variable, not tensor
 y_relu = F.relu(x).data.numpy()
 y_sigmoid = F.sigmoid(x).data.numpy()
 y_tanh = F.tanh(x).data.numpy()
 y_softplus = F.softplus(x).data.numpy()
+# F.softmax(x),  x has to be 2-d tensor
 # y_softmax = F.softmax(x)  softmax is a special kind of activation function, it is about probability
 
 
-# plt to visualize these activation function
+# create a single figure with a particular size
 plt.figure(1, figsize=(8, 6))
+# create a subplot with position inside the figure
 plt.subplot(221)
+# plot line with color and label
 plt.plot(x_np, y_relu, c='red', label='relu')
+# set y limit
 plt.ylim((-1, 5))
+# set legend with location
 plt.legend(loc='best')
 
 plt.subplot(222)
