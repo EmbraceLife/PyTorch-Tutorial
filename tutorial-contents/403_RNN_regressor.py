423a3445943e2406b54fb418e29e1c69bc7020c9
EmbraceLife <1227561934@qq.com>
2017-05-28 21:30:10 +0800

    simplify

--- tutorial-contents/403_RNN_regressor.py
+++ tutorial-contents/403_RNN_regressor.py
@@ -28,11 +28,14 @@ import matplotlib.pyplot as plt
 
 torch.manual_seed(1)    # reproducible
 
+############################
 # Hyper Parameters
 TIME_STEP = 10      # rnn time step
 INPUT_SIZE = 1      # rnn input size
 LR = 0.02           # learning rate
+train_again = True  # to use previous trained model and loss
 
+############################
 # show data
 steps = np.linspace(0, np.pi*2, 100, dtype=np.float32)
 x_np = np.sin(steps)    # float32 for converting torch FloatTensor
@@ -42,7 +45,8 @@ plt.plot(steps, x_np, 'b-', label='input (sin)')
 plt.legend(loc='best')
 plt.show()
 
-
+############################
+## Create RNN class
 class RNN(nn.Module):
     def __init__(self):
         super(RNN, self).__init__()
@@ -61,7 +65,7 @@ class RNN(nn.Module):
         # r_out (batch, time_step, hidden_size)
         r_out, h_state = self.rnn(x, h_state)
 
-		# How to understand here? every step's output is applied to self.out layer, then stored into a list, then stacked into a tensor with shape (1, 10, 1)
+		# every step's output is applied to self.out layer, then stored into a list, then stacked into a tensor with shape (1, 10, 1)
 		# 10 is num of steps
         outs = []    # save all predictions
         for time_step in range(r_out.size(1)):
@@ -70,43 +74,58 @@ class RNN(nn.Module):
         return torch.stack(outs, dim=1), h_state
 
 
-rnn = RNN()
-print(rnn)
+############################
+## Build graph
 
+## load rnn or create rnn from scratch
+if train_again:
+	rnn = torch.load("/Users/Natsume/Downloads/temp_folders/403/rnn.pkl")
+else:
+	rnn = RNN()
+	print(rnn)
 
 optimizer = torch.optim.Adam(rnn.parameters(), lr=LR)   # optimize all cnn parameters
 loss_func = nn.MSELoss()
 
-h_state = None      # for initial hidden state
 
+############################
+## Training
+
+# set initial hidden state, to carry it onto next batch of training
+h_state = None
+
+# start plotting framework
 plt.figure(1, figsize=(12, 5))
 plt.ion()           # continuously plot
 plt.show()
 
+
+## training loop
 for step in range(60):
+
+	## each loop, create a full steps of dataset
     start, end = step * np.pi, (step+1)*np.pi   # time range
     # use sin predicts cos
     steps = np.linspace(start, end, TIME_STEP, dtype=np.float32)
     x_np = np.sin(steps)    # float32 for converting torch FloatTensor
     y_np = np.cos(steps)
 
-	## convert a 1-d array to 3d (batch, step, input_size)
-		# add 2 dimensions at 0 and 2 positions
+	## make dataset variables and get shape right
     x = Variable(torch.from_numpy(x_np[np.newaxis, :, np.newaxis]))    # shape (batch, time_step, input_size)
     y = Variable(torch.from_numpy(y_np[np.newaxis, :, np.newaxis]))
 
+	## forward pass
     prediction, h_state = rnn(x, h_state)   # rnn output
-
-    # !! next step is important !!
-	# repack the hidden state, break the connection from last iteration
-    h_state = Variable(h_state.data)
-
     loss = loss_func(prediction, y)         # cross entropy loss
     optimizer.zero_grad()                   # clear gradients for this training step
     loss.backward()                         # backpropagation, compute gradients
     optimizer.step()                        # apply gradients
 
-    # plotting
+    # !! next step is important !!
+	# repack the hidden state, break the connection from last iteration
+    h_state = Variable(h_state.data)
+
+    # plotting a step of dataset (both prediction and y_np)
     plt.plot(steps, y_np.flatten(), 'r-')
     plt.plot(steps, prediction.data.numpy().flatten(), 'b-')
     # plt.draw() # not really necessary
@@ -114,3 +133,5 @@ for step in range(60):
 
 plt.ioff()
 plt.show()
+
+torch.save(rnn, "/Users/Natsume/Downloads/temp_folders/403/rnn.pkl")
--- tutorial-contents/404_autoencoder.py
+++ tutorial-contents/404_autoencoder.py
@@ -44,33 +44,33 @@ DOWNLOAD_MNIST = False
 N_TEST_IMG = 5
 
 ###################################
-## Load dataset, plot it, and batch them
-# Mnist digits dataset
+## Load dataset
+# do check doc for this func
 train_data = torchvision.datasets.MNIST(
     root='./mnist/',
-    train=True,                                     # this is training data
-    transform=torchvision.transforms.ToTensor(),    # Converts a PIL.Image or numpy.ndarray to
-                                                    # torch.FloatTensor of shape (C x H x W) and normalize in the range [0.0, 1.0]
-    download=DOWNLOAD_MNIST,                        # download it if you don't have it
+    train=True,
+    transform=torchvision.transforms.ToTensor(),
+    download=DOWNLOAD_MNIST,
 )
 
-# plot one example
+## plot one example
 print(train_data.train_data.size())     # (60000, 28, 28)
 print(train_data.train_labels.size())   # (60000)
 plt.imshow(train_data.train_data[2].numpy(), cmap='gray')
 plt.title('%i' % train_data.train_labels[2])
 plt.show()
 
-# Data Loader for easy mini-batch return in training, the image batch shape will be (50, 1, 28, 28)
+## make batch
+# explore object: train_loader
 train_loader = Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
 
 #############################
 ## build autoEncoder class:
-# contain 2 attributes: encoder, decoder (they are Sequential class)
 class AutoEncoder(nn.Module):
     def __init__(self):
         super(AutoEncoder, self).__init__()
 
+		# create encoder: a sequential object
         self.encoder = nn.Sequential(
             nn.Linear(28*28, 128),
             nn.Tanh(),
@@ -78,8 +78,11 @@ class AutoEncoder(nn.Module):
             nn.Tanh(),
             nn.Linear(64, 12),
             nn.Tanh(),
-            nn.Linear(12, 3),   # compress to 3 features which can be visualized in plt
+            nn.Linear(12, 3),
+			# encoder or shrink down to 3 features
         )
+
+		# create decoder: a sequential object
         self.decoder = nn.Sequential(
             nn.Linear(3, 12),
             nn.Tanh(),
@@ -88,9 +91,10 @@ class AutoEncoder(nn.Module):
             nn.Linear(64, 128),
             nn.Tanh(),
             nn.Linear(128, 28*28),
-            nn.Sigmoid(),       # compress to a range (0, 1)
+            nn.Sigmoid(),
         )
 
+	# apply dataset onto this neuralnet
     def forward(self, x):
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
@@ -101,20 +105,21 @@ class AutoEncoder(nn.Module):
 # instantiate autoEncoder
 autoencoder = AutoEncoder()
 optimizer = torch.optim.Adam(autoencoder.parameters(), lr=LR)
+# mse: not compare labels (crossentropy), but compare image values
 loss_func = nn.MSELoss()
 
 
 ############################
-## plot the first 5 images as subplots
-# initialize figure: another way of subplotting (2 rows, 5 cols)
+## create images comparison framework
+# subplotting (2 rows, 5 cols)
 f, a = plt.subplots(2, N_TEST_IMG, figsize=(5, 2))
 plt.ion()   # continuously plot
 plt.show()
 
-# get N_TEST_IMG (5 here) images data to work on
+# prepare 5 images for plotting and make them a Variable
 view_data = Variable(train_data.train_data[:N_TEST_IMG].view(-1, 28*28).type(torch.FloatTensor)/255.)
 
-# easy and fast way of plotting the 5 subplots on a row
+# plot 5 images on a same row
 for i in range(N_TEST_IMG):
     a[0][i].imshow(np.reshape(view_data.data.numpy()[i], (28, 28)), cmap='gray')
     a[0][i].set_xticks(())
@@ -125,14 +130,14 @@ for i in range(N_TEST_IMG):
 # for each epoch
 for epoch in range(EPOCH):
 
-	# for each batch, batch x and batch label
+	# for each batch, batch features and batch label
     for step, (x, y) in enumerate(train_loader):
 		# first, check x, y, size
 		# x.size(): (64, 1, 28, 28)
 
-		# batch x, shape (batch, 28*28)
+		# make batch x a Variable, shape (batch, 28*28)
         b_x = Variable(x.view(-1, 28*28))
-		# batch y, shape (batch, 28*28)
+		# make batch y a Variable, shape (batch, 28*28)
         b_y = Variable(x.view(-1, 28*28))
 		# batch label
         b_label = Variable(y)
@@ -140,20 +145,21 @@ for epoch in range(EPOCH):
 
 		## actual forward pass
         encoded, decoded = autoencoder(b_x)
-        loss = loss_func(decoded, b_y)      # mean square error
+		## loss on mse, using no labels
+        loss = loss_func(decoded, b_y)
 
 		## actual backward pass
-        optimizer.zero_grad()               # clear gradients for this training step
-        loss.backward()                     # backpropagation, compute gradients
-        optimizer.step()                    # apply gradients
+        optimizer.zero_grad() # clear gradients
+        loss.backward() # update gradients
+        optimizer.step() # update weights
 
-		## Every 100 batches of training
+		## after 100 batches training, log and plot decoder images
         if step % 100 == 0:
 
-			# print out loss with epoch index
-            print('Epoch: ', epoch, '| train loss: %.4f' % loss.data[0])
+			# print: epoch | batch | loss
+            print('Epoch: ', epoch, '| Batch: ', step, '| train loss: %.4f' % loss.data[0])
 
-            # plotting decoded image (second row) on the first 5 images above
+            # plotting decoded image on second row
             _, decoded_data = autoencoder(view_data)
             for i in range(N_TEST_IMG):
                 a[1][i].clear()
@@ -168,12 +174,15 @@ plt.show()
 
 
 ##########################
-# visualize in 3D plot
+## visualize in 3D plot
+# not sure the need for 3D plotting yet????
+
 
 ## prepare 200 images
 view_data = Variable(train_data.train_data[:200].view(-1, 28*28).type(torch.FloatTensor)/255.)
 
 ## encode these 200 images
+# encoder shape (200, 3)
 encoded_data, _ = autoencoder(view_data)
 
 # create a second plot
