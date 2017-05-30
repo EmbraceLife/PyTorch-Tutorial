6a93d7a79ced004f9d8c348f7303da7e561ba404
EmbraceLife <1227561934@qq.com>
2017-05-30 16:57:00 +0800

    update variable and activation

--- tutorial-contents/202_variable.py
+++ tutorial-contents/202_variable.py
@@ -2,43 +2,58 @@ import torch
 from torch.autograd import Variable
 
 tensor = torch.FloatTensor([[1,2],[3,4]])
-
-# check var.grad
 variable_false = Variable(tensor) # can't compute gradients
 variable_true = Variable(tensor, requires_grad=True)
 
-# add operations
+# tensor operations
 t_out = torch.mean(tensor*tensor)
-
+# variable operations
 v_out_false = torch.mean(variable_false*variable_false)
 v_out_true = torch.mean(variable_true*variable_true)
 
+"""
 # backpropagation
+v_out_false.backward()
 # RuntimeError: there are no graph nodes that require computing gradients, due to `requires_grad=False`
-# v_out_false.backward()
-v_out_true.backward()
+"""
+# to do backward for a second or third time, must keep `retain_variables=True` every time when call backward
+v_out_true.backward(retain_variables=True)
+# equivalent to v_out_true.backward(torch.ones(1))
 print(variable_true.grad)
 print(v_out_true.creator)
+v_out_true.backward(retain_variables=True)
+# every time call backward() will change the gradients
+print(variable_true.grad)
+print(v_out_true.creator)
+# use torch.autograd's backward
+v_out_true.backward(torch.ones(1), retain_variables=True)
+print(variable_true.grad)
 
-x = Variable(torch.ones(2, 2), requires_grad=True)
-y = x + 2
-print(y.creator)
+"""
+Variable containing:
+ 0.5000  1.0000
+ 1.5000  2.0000
+[torch.FloatTensor of size 2x2]
 
-z = y * y * 3
-out = z.mean()
-out.backward() # equivalent to out.backward(torch.Tensor([1.0]))
-print(x.grad)
+<torch.autograd._functions.reduce.Mean object at 0x116a159e8>
+Variable containing:
+ 1  2
+ 3  4
+[torch.FloatTensor of size 2x2]
+
+<torch.autograd._functions.reduce.Mean object at 0x116a159e8>
+Variable containing:
+ 1.5000  3.0000
+ 4.5000  6.0000
+[torch.FloatTensor of size 2x2]
+"""
 
 x = torch.randn(3)
 x = Variable(x, requires_grad=True)
 y = x * 2
+# we can set gradients freely I think?
+gradients = torch.FloatTensor([0.1, 1.0, 0.0001])
 while y.data.norm() < 1000:
     y = y * 2
-
-
-# if grad is scalar, we can use y.backward(), if not, just provide a tensor with same size as gradients
-# with grad value will be multiplied with gradients tensor provided here
-gradients = torch.FloatTensor([0.1, 1.0, 0.0001])
-y.backward(gradients)
-print(x.grad)
-print(gradients)
+    y.backward(gradients, retain_variables=True)
+    print(x.grad)
--- tutorial-contents/203_activation.py
+++ tutorial-contents/203_activation.py
@@ -1,58 +1,49 @@
-"""
-View more, visit my tutorial page: https://morvanzhou.github.io/tutorials/
-My Youtube Channel: https://www.youtube.com/user/MorvanZhou
-
-Dependencies:
-torch: 0.1.11
-matplotlib
-"""
 import torch
-# with torch.nn.functional as F, we can quickly deploy activations to use
-# to see detailed doc of activation, we need
-# import torch.nn as nn
-# doc nn.ReLU
 import torch.nn.functional as F
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
 
-# torch.linspace(range_start, range_end, total_num)
-# it is 1-d tensor
-x = torch.linspace(-5, 5, 200)  # x data (tensor), shape=(100, 1)
+## apply activations to a 1d variable
+x = torch.linspace(-5, 5, 200)
 x = Variable(x)
-x_np = x.data.numpy()   # numpy array for plotting
 
-# apply activation only to a Variable, not tensor
-y_relu = F.relu(x).data.numpy()
-y_sigmoid = F.sigmoid(x).data.numpy()
-y_tanh = F.tanh(x).data.numpy()
-y_softplus = F.softplus(x).data.numpy()
-# F.softmax(x),  x has to be 2-d tensor
-# y_softmax = F.softmax(x)  softmax is a special kind of activation function, it is about probability
+# only variables can be applied with activations
+y_relu = F.relu(x)
+y_sigmoid = F.sigmoid(x)
+y_tanh = F.tanh(x)
+y_softplus = F.softplus(x)
 
+# F.softmax only take 2d variable
+x1 = x.view((-1,5))
+y_softmax = F.softmax(x1)
 
-# create a single figure with a particular size
-plt.figure(1, figsize=(8, 6))
-# create a subplot with position inside the figure
+"""
+# checkout all functions availabe in F
+dt F
+# check out documents of F.softmax
+sources torch.nn.Softmax
+"""
+x_np = x.data.numpy()
+## plotting activations
+plt.figure(1, figsize=(5, 3))
 plt.subplot(221)
-# plot line with color and label
-plt.plot(x_np, y_relu, c='red', label='relu')
-# set y limit
+# plot relu
+plt.plot(x_np, y_relu.data.numpy(), c='red', label='relu')
 plt.ylim((-1, 5))
-# set legend with location
 plt.legend(loc='best')
-
+# plot sigmoid
 plt.subplot(222)
-plt.plot(x_np, y_sigmoid, c='red', label='sigmoid')
+plt.plot(x_np, y_sigmoid.data.numpy(), c='red', label='sigmoid')
 plt.ylim((-0.2, 1.2))
 plt.legend(loc='best')
-
+# plot tanh
 plt.subplot(223)
-plt.plot(x_np, y_tanh, c='red', label='tanh')
+plt.plot(x_np, y_tanh.data.numpy(), c='red', label='tanh')
 plt.ylim((-1.2, 1.2))
 plt.legend(loc='best')
-
+# plot softplus
 plt.subplot(224)
-plt.plot(x_np, y_softplus, c='red', label='softplus')
+plt.plot(x_np, y_softplus.data.numpy(), c='red', label='softplus')
 plt.ylim((-0.2, 6))
 plt.legend(loc='best')
 
