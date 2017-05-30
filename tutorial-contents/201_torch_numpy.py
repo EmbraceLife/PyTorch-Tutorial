88e404b2498dae3bd7fcc4968c6f959499f489de
EmbraceLife <1227561934@qq.com>
2017-05-30 11:16:44 +0800

    update torch_numpy

--- tutorial-contents/201_torch_numpy.py
+++ tutorial-contents/201_torch_numpy.py
@@ -1,77 +1,73 @@
-"""
-View more, visit my tutorial page: https://morvanzhou.github.io/tutorials/
-My Youtube Channel: https://www.youtube.com/user/MorvanZhou
-
-Dependencies:
-torch: 0.1.11
-numpy
-"""
 import torch
 import numpy as np
 
-# details about math operation in torch can be found in: http://pytorch.org/docs/torch.html#math-operations
-
-# create a simple 2-d numpy array
+# from np.array to torch.tensor, and backforth
 np_data = np.arange(6).reshape((2, 3))
-
-# convert numpy array to pytorch tensor
 torch_data = torch.from_numpy(np_data)
-
-# convert a pytorch tensor to numpy array
 tensor2array = torch_data.numpy()
-print(
-    '\nnumpy array:', np_data,          # [[0 1 2], [3 4 5]]
-    '\ntorch tensor:', torch_data,      #  0  1  2 \n 3  4  5    [torch.LongTensor of size 2x3]
-    '\ntensor to array:', tensor2array, # [[0 1 2], [3 4 5]]
-)
 
-
-# create a pytorch tensor from a list
+# from list to FloatTensor
 data = [-1, -2, 1, 2]
 tensor = torch.FloatTensor(data)  # 32-bit floating point
-print(
-    '\nabs',
-	# numpy and pytorch operations: abs
-    '\nnumpy: ', np.abs(data),          # [1 2 1 2]
-    '\ntorch: ', torch.abs(tensor)      # [1 2 1 2]
-)
 
-# sin
-print(
-    '\nsin',
-	# # numpy and pytorch operations: sin
-    '\nnumpy: ', np.sin(data),      # [-0.84147098 -0.90929743  0.84147098  0.90929743]
-    '\ntorch: ', torch.sin(tensor)  # [-0.8415 -0.9093  0.8415  0.9093]
-)
+# operations on numpy and tensor
+np.abs(data)
+torch.abs(tensor)
 
-# mean
-print(
-    '\nmean',
-	# numpy and pytorch operations: mean
-	# torch.mean(a, 0): to shrink rows
-	# torch.mean(a, 1): to shrink cols
-    '\nnumpy: ', np.mean(data),         # 0.0
-    '\ntorch: ', torch.mean(tensor)     # 0.0
-)
+np.sin(data)
+torch.sin(tensor)
+
+# numpy and pytorch operations: mean
+# torch.mean(a, 0): to shrink rows
+# torch.mean(a, 1): to shrink cols
+np.mean(data)
+torch.mean(tensor)
 
 # matrix multiplication
-# create 2-d pytorch tensor from list
 data = [[1,2], [3,4]]
 tensor = torch.FloatTensor(data)  # 32-bit floating point
-# correct method
-print(
-    '\nmatrix multiplication (matmul)',
-	# numpy, pytorch matrix multiplication
-	# (1, 4), (4, 5) = (1, 5)
-	# dot(row_tensor1, col_tensor2) = multiply and sum
-    '\nnumpy: ', np.matmul(data, data),     # [[7, 10], [15, 22]]
-    '\ntorch: ', torch.mm(tensor, tensor)   # [[7, 10], [15, 22]]
-)
+# numpy, pytorch matrix multiplication
+# (1, 4), (4, 5) = (1, 5)
+# dot(row_tensor1, col_tensor2) = multiply and sum
+np.matmul(data, data),     # [[7, 10], [15, 22]]
+torch.mm(tensor, tensor)   # [[7, 10], [15, 22]]
+
 # incorrect method
 data = np.array(data)
-print(
-    '\nmatrix multiplication (dot)',
-	# dot operation means different things for pytorch and numpy
-    '\nnumpy: ', data.dot(data),        # [[7, 10], [15, 22]]
-    '\ntorch: ', tensor.dot(tensor)     # tensor will be flatten to 1-d, so you'll get 30.0
-)
+data.dot(data)        # [[7, 10], [15, 22]]
+tensor.dot(tensor)     # tensor will be flatten to 1-d, so you'll get 30.0
+
+###################### based on Pytorch official tutorials
+# Construct a 5x3 matrix, uninitialized:
+x = torch.Tensor(5, 3)
+# Construct a randomly initialized matrix
+x = torch.rand(5, 3)
+y = torch.rand(5, 3)
+print(x + y)
+print(torch.add(x, y))
+# Addition: giving an output tensor
+result = torch.Tensor(5, 3)
+torch.add(x, y, out=result)
+
+y.add_(x)  # Addition: in-place
+x.copy_(y)
+x.t_()     # all these will change ``x``.
+
+# You can use standard numpy-like indexing with all bells and whistles!
+print(x[:, 1])
+
+## when tensor a changes, numpy b changes too
+a = torch.ones(5)
+b = a.numpy()
+a.add_(1)
+
+## when numpy array a changes, tensor b changes too
+a = np.ones(5)
+b = torch.from_numpy(a)
+np.add(a, 1, out=a)
+
+## use cuda
+if torch.cuda.is_available():
+    x = x.cuda()
+    y = y.cuda()
+    x + y
